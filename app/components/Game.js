b8829dce0c7d8490fb3d9492da3c085d26192327
Tarallo <tarantinofranc3sco@gmail.com>
2023-10-21 13:41:18 +0200

    mini game

--- app/components/Game.js
+++ app/components/Game.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 
 export default function Game() {
   const [username, setUsername] = useState("");
   const [characterClass, setCharacterClass] = useState("");
+  const [weapon, setWeapon] = useState("");
   const [inputText, setInputText] = useState("");
   const [displayText, setDisplayText] = useState("");
   const [isTyping, setIsTyping] = useState(false);
   const [chatHistory, setChatHistory] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [choices, setChoices] = useState([]);
+  const [selectedChoiceIndex, setSelectedChoiceIndex] = useState(null);
 
   const getQuestionText = (index) => {
-    if (index === 0) {
-      return `Welcome to this mini game! What's your username?`;
-    } else if (index === 1) {
+
+    if (index === 1) {
       return `What a nice name, ${username || "Player"}! What class would you like to choose?`;
     } else if (index === 2) {
       return `I understand you chose ${characterClass}. Now let's start our adventure!`
-    }
-    else if (index === 3 ) {
-      return `...`;
-    }
-    else if (index === 4) {
+    } else if (index === 4) {
       return `You wake up unconscious in a room with some weapons. What do you choose, ${username || "Player"}?`;
+    } else if(index === 5) {
+      return `You chose a ${weapon}. That will help us during our adventure.`;
     } else {
       return questions[index];
     }
   };
 
   const questions = [
+    "Welcome to this mini game! What's your username?",
     "What class would you like to choose?",
     "I understand. Now let's start our adventure!",
     "...",
-    "You wake up unconscious in a room with some weapons. What do you choose"
+    "You wake up unconscious in a room with some weapons. What do you choose",
+    "Nice weapon!",
   ];
 
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [choices, setChoices] = useState([]);
-  const [selectedChoiceIndex, setSelectedChoiceIndex] = useState(null);
+  
 
   useEffect(() => {
     if (currentQuestionIndex < questions.length) {
@@ -60,27 +61,40 @@ export default function Game() {
   }, [currentQuestionIndex]);
   
 
-  const handleInputChange = (e) => {
-    const text = e.target.value;
-    setInputText(text);
-
-    if (currentQuestionIndex === 0) {
-      setUsername(text);
-    } else if (currentQuestionIndex === 1) {
-      setCharacterClass(text);
-    } else if (currentQuestionIndex === 2 || currentQuestionIndex === 3) {
-      const text = "...";
-      setInputText(text);
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+    if (currentQuestionIndex === 0 || currentQuestionIndex === 2 || currentQuestionIndex === 3 || currentQuestionIndex === 5) {
+      setUsername(event.target.value);
     }
+    
+    if (currentQuestionIndex === 1) {
+      setCharacterClass(event.target.value);
+    }
+    
+    if (currentQuestionIndex === 4) {
+      setWeapon(event.target.value);
+    } 
   };
+  
 
   const handleChoiceClick = (choice, index) => {
     setSelectedChoiceIndex(index);
-    setCharacterClass(choice);
+  
+    if (currentQuestionIndex === 4) {
+      // If this is the weapon selection
+      setWeapon(choice);
+      handleNextQuestion();
+    } else {
+      // Else for now it will be the character class selection
+      setCharacterClass(choice);
+      handleNextQuestion();
+    }
   };
 
+  
   const handleNextQuestion = () => {
     const currentQuestion = getQuestionText(currentQuestionIndex);
+  
     if (currentQuestionIndex === 0 && username) {
       setChatHistory([
         ...chatHistory,
@@ -95,28 +109,19 @@ export default function Game() {
       ]);
       setCurrentQuestionIndex(2);
       setChoices([]);
-      setSelectedChoiceIndex(null);
     } else if (currentQuestionIndex === 2) {
       setChatHistory([...chatHistory, { question: currentQuestion }]);
       setCurrentQuestionIndex(3);
     } else if (currentQuestionIndex === 3) {
-      setInputText("...");
       setChatHistory([...chatHistory, { question: currentQuestion }]);
       setCurrentQuestionIndex(4);
       setChoices(["🧪 Potion", "🔫 Gun", "🗡️ Sword", "💎 Rock"]);
-    } else if (currentQuestionIndex === 4) {
+    } else if (currentQuestionIndex === 4 && weapon) {
       setChatHistory([
         ...chatHistory,
-        { question: currentQuestion, answer: inputText },
+        { question: currentQuestion, answer: weapon },
       ]);
-      setChoices([]);
-      setSelectedChoiceIndex(null);
-  
-      if (currentQuestionIndex === questions.length - 1) {
-        // End the conversation
-      } else {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-      }
+      setCurrentQuestionIndex(5);
     }
   };
   
@@ -130,7 +135,7 @@ export default function Game() {
             <span className="typing-effect">{displayText}</span>
           </div>
           <div className="user-response">
-            {currentQuestionIndex === 1 || currentQuestionIndex === 2 || currentQuestionIndex === 3 || currentQuestionIndex === 4 ? (
+            {currentQuestionIndex === 1 || currentQuestionIndex === 2 || currentQuestionIndex === 3 || currentQuestionIndex === 4 || currentQuestionIndex === 5 ? (
               <div className="questions-buttons">
                 {choices.map((choice, index) => (
                   <button
@@ -147,7 +152,7 @@ export default function Game() {
               <input
                 type="text"
                 placeholder="Your response"
-                value={currentQuestionIndex === 0 ? username : characterClass}
+                value={inputText}
                 onChange={handleInputChange}
                 disabled={isTyping}
                 className="user-input"
@@ -164,7 +169,7 @@ export default function Game() {
         <div className="game-textarea">
           <div className="chat-history">
             <h3>💬 Chatlog</h3>
-            {chatHistory.map((chat, index) => (
+            {chatHistory.slice().reverse().map((chat, index) => (
               <div key={index}>
                 <div className="chat-bot">{chat.question}</div>
                 {chat.answer ? (
