2eed38cd578acdf3cc1423bd59740351bb3f5903
Tarallo <tarantinofranc3sco@gmail.com>
2023-10-22 12:53:48 +0200

    mini game

--- app/components/Game.js
+++ app/components/Game.js
@@ -14,6 +14,18 @@ export default function Game() {
   const [selectedChoiceIndex, setSelectedChoiceIndex] = useState(null);
   const [userChoice, setUserChoice] = useState("");
   const hearts = Array(userLife).fill('❤️');
+  const [enemy, setEnemy] = useState("");
+  const [enemyLife, setEnemyLife] = useState(10);
+  const [attack, setAttack] = useState("1"); // Minimum attack value (for no weapon cases) set to 1
+  const [enemyAttack, setEnemyAttack] = useState("");
+
+  const enemies = [
+    "🧟 ZOMBIE",
+    "🧌 CORRUPTED WITCH",
+    "👹 WANDERING SPIRIT",
+    "🐺 EVIL WEREWOLF",
+  ]
+
 
   const goals = [
     "to destroy all the zobies on Earth",
@@ -23,6 +35,8 @@ export default function Game() {
   ]
 
   const goal = goals[Math.floor(Math.random() * goals.length)];
+  
+  
 
   const getQuestionText = (index) => {
 
@@ -39,10 +53,32 @@ export default function Game() {
     }else if(index === 8) {
       return `You chose a ${weapon}. That will help us during our adventure.`;
     } else if(index === 12) {
-      return `Oh no! ${username} you encounter a fearsome 🧟 zombie! What will you do?`;
+      return `Oh no! ${username} you encounter a ${enemy} (${enemyLife} life points)! What will you do?`;
     } else if(index === 13) {
       return `Hurry up ${username} and do something before we get attacked!`;
-    } else {
+    } else if(index === 14) {
+      return `Well played ${username}! Your hit inflicted ${attack} damage on the ${enemy}.`;
+    } else if(index === 15) {
+      return `Oh no, ${enemy} (${enemyLife} life points) gets angry and hits you. `;
+    }else if(index === 16) {
+      return `${enemy} hurls himself at you, luckily you manage to dodge in time. However, this inflicts ${enemyAttack} damage on you.`;
+    }else if(index === 17) {
+      return `${enemy} is in front of you and it is about to get up from the ground! Hurry up ${username}, what do you choose to do?`;
+    }else if(index === 18) {
+      return `${enemy} is still alive (${enemyLife} life points)! It suddenly attacks you!`;
+    } else if(index === 19) {
+      return `${enemy} fails to hit you. It inflicts ${enemyAttack} damage on you!`;
+    } else if(index === 20) {
+      return `${enemy} is still alive (${enemyLife} life points). What do you want to do ${username}?`;
+    }else if(index === 21) {
+      return `Great! You inflicted ${attack} damage.`;
+    }else if(index === 22) { // If user is dead
+      return `Oh no ${username}. ${enemy} defeated you.`;
+    }else if(index === 23) { // If user defeat enemy
+      return `Well done ${username}! ${enemy} has been defeated.`;
+    }else if(index === 24) { // Going out the fight 
+      return `You notice that in the northeast direction you can see smoke, as if someone has lit a fire. You decide to head to that area.`;
+    }else {
       return questions[index];
     }
   };
@@ -59,9 +95,23 @@ export default function Game() {
     "Nice weapon!",
     "You find the exit and are out of the building.",
     "You see ahead of you destroyed buildings, everything looks abandoned. You see on the horizon the sunset. It is almost evening.",
-    "Wait a moment--do you smell that strange smell? Where is it coming from?",
+    "Wait a moment--do you hear that? Where is it coming from?!",
     "first enemy",
     "first action fight",
+    "action result",
+    "Enemy action",
+    "User receives damage",
+    "User second action",
+    "Is enemy dead?",
+    "Enemy second action",
+    "Last chance to beat enemy",
+    "If enemy still alive",
+    "User last action",
+    "If user Dead",
+    "If enemy beaten",
+    "You go out the fight",
+    "If escaped ",
+    "If fight ended",
   ];
 
 
@@ -111,11 +161,37 @@ export default function Game() {
   const handleChoiceClick = (choice, index) => {
     setSelectedChoiceIndex(index);
 
-    if (currentQuestionIndex === 6 || currentQuestionIndex === 12 || currentQuestionIndex === 13) {
+    if (currentQuestionIndex === 6 || currentQuestionIndex === 12) {
       //
       setUserChoice(choice);
       
-    } else if (currentQuestionIndex === 7) {
+    } else if (currentQuestionIndex === 13 || currentQuestionIndex === 17) {
+      setUserChoice(choice);
+      if (choice === "Kick") {
+        // Define damage for the Kick action
+        const kickDamage = 3;
+        setEnemyLife(enemyLife - kickDamage);
+        setAttack(kickDamage);
+        
+      } else if (choice === "Punch") {
+        // Define damage for the Punch action
+        const punchDamage = 2;
+        setEnemyLife(enemyLife - punchDamage);
+        setAttack(punchDamage);
+      } else if (choice === weapon) {
+        // Implement logic for using the user's weapon
+        // Define damage for the weapon action
+        const weaponDamage = 5; 
+        setAttack(weaponDamage);
+        
+        setEnemyLife(enemyLife - weaponDamage);
+      } else if (choice === "Escape") {
+        // Handle the user's escape action (if needed)
+        // You can add escape logic here
+      }
+
+    }
+    else if (currentQuestionIndex === 7) {
       // If this is the weapon selection
       setWeapon(choice);
     } else { 
@@ -207,6 +283,11 @@ export default function Game() {
         { question: currentQuestion },
       ]);
       setCurrentQuestionIndex(12);
+      // First enemy encounter
+      const randomEnemy = enemies[Math.floor(Math.random() * enemies.length)];
+      const randomEnemyLife = Math.floor(Math.random() * 10 + 5);
+      setEnemyLife(randomEnemyLife);
+      setEnemy(randomEnemy);
       setChoices(["🏃 Escape", "🤺 Fight"]);
     } else if (currentQuestionIndex === 12) {
       setChatHistory([
@@ -214,14 +295,119 @@ export default function Game() {
         { question: currentQuestion, answer: userChoice },
       ]);
       if(userChoice === "🤺 Fight") {
-        setCurrentQuestionIndex(13)
-        setChoices([`Kick`, `Punch`, `${weapon}`, `Escape`]);
+        setCurrentQuestionIndex(13);
+      } else if (userChoice === "🏃 Escape") {
+        setCurrentQuestionIndex(23);
+      }
+      setChoices([`Kick`, `Punch`, `${weapon}`, `Escape`]);
+    } else if (currentQuestionIndex === 13) {
         setChatHistory([
           ...chatHistory,
           { question: currentQuestion, answer: userChoice },
         ]);
+      setCurrentQuestionIndex(14); 
+      setChoices([]);
+    } else if (currentQuestionIndex === 14) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  },
+      ]);
+    setCurrentQuestionIndex(15); 
+    } else if (currentQuestionIndex === 15) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  },
+      ]);
+      // Enemy Attack
+      const randomAttack = Math.floor(Math.random() * 2 + 1);
+    setEnemyAttack(randomAttack);
+    setUserLife(userLife - randomAttack);
+    setCurrentQuestionIndex(16); 
+    } else if (currentQuestionIndex === 16) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  answer:`${userLife} life points left`},
+      ]);
+    setCurrentQuestionIndex(17);
+    setChoices([`Kick`, `Punch`, `${weapon}`, `Escape`]);
+    } else if (currentQuestionIndex === 17) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  answer: userChoice},
+      ]);
+      if(enemyLife <= 0){
+        setCurrentQuestionIndex(23);
+      } else {
+        // Enemy 2nd Attack
+        const randomAttack = Math.floor(Math.random() * 2);
+        setEnemyAttack(randomAttack);
+        setUserLife(userLife - randomAttack);
+        setCurrentQuestionIndex(18);
       }
-    }
+      setChoices([]);
+    } else if (currentQuestionIndex === 18){
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  answer:`${userLife} life points left`},
+      ]);
+      if(userLife <= 0) {
+        setCurrentQuestionIndex(22);
+      } else {
+        setCurrentQuestionIndex(19);
+      }
+      
+    } else if (currentQuestionIndex === 19) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion},
+      ]);
+      setChoices([`Kick`, `Punch`, `${weapon}`, `Escape`]);
+      setCurrentQuestionIndex(20); 
+    } else if (currentQuestionIndex === 20) {
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,  answer: userChoice},
+      ]);
+      if(enemyLife <= 0){
+        setCurrentQuestionIndex(23);
+      } else {
+        // Enemy 2nd Attack
+        const randomAttack = Math.floor(Math.random() * 2);
+        setEnemyAttack(randomAttack);
+        setUserLife(userLife - randomAttack);
+        setCurrentQuestionIndex(21);
+      }
+      setChoices([]);
+    } else if (currentQuestionIndex === 21){
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,},
+      ]);
+      if(userLife <= 0) {
+        setCurrentQuestionIndex(22);
+      } else {
+        setCurrentQuestionIndex(23);
+      }
+    }  else if (currentQuestionIndex === 22){
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,},
+      ]); // User is defeated case
+      setCurrentQuestionIndex(22);
+    } else if (currentQuestionIndex === 23){
+      // Enemy is defeated
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,},
+      ]);
+      setCurrentQuestionIndex(24);
+    } else if (currentQuestionIndex === 24){
+      setChatHistory([
+        ...chatHistory,
+        { question: currentQuestion,},
+      ]);
+      setCurrentQuestionIndex(25);
+    } 
   };
   
 
@@ -244,7 +430,7 @@ export default function Game() {
             <span className="typing-effect">{displayText}</span>
           </div>
           <div className="user-response">
-            {currentQuestionIndex === 1 || currentQuestionIndex === 2 || currentQuestionIndex === 3 || currentQuestionIndex === 4 || currentQuestionIndex === 5 || currentQuestionIndex === 6 || currentQuestionIndex === 7 || currentQuestionIndex === 8 || currentQuestionIndex === 9 || currentQuestionIndex === 10 || currentQuestionIndex === 11 || currentQuestionIndex === 12 || currentQuestionIndex === 13? (
+            {currentQuestionIndex === 1 || currentQuestionIndex === 2 || currentQuestionIndex === 3 || currentQuestionIndex === 4 || currentQuestionIndex === 5 || currentQuestionIndex === 6 || currentQuestionIndex === 7 || currentQuestionIndex === 8 || currentQuestionIndex === 9 || currentQuestionIndex === 10 || currentQuestionIndex === 11 || currentQuestionIndex === 12 || currentQuestionIndex === 13 || currentQuestionIndex === 14 || currentQuestionIndex === 15 || currentQuestionIndex === 16 || currentQuestionIndex === 17 || currentQuestionIndex === 18 || currentQuestionIndex === 19 || currentQuestionIndex === 20 || currentQuestionIndex === 21 || currentQuestionIndex === 22 || currentQuestionIndex === 23 || currentQuestionIndex === 24? (
               <div className="questions-buttons">
                 {choices.map((choice, index) => (
                   <button
